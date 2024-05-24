3f7e023165aa24c035a62a2d172405c02139ddbd
carolinagonta <carolinagtaa@gmail.com>
2024-05-24 22:58:46 +0300

    Update review

--- eUseControl.BusinessLogic/DBModel/ReviewsContext.cs
+++ eUseControl.BusinessLogic/DBModel/ReviewsContext.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+using eUseControl.Domain.Entities.User;
+
+namespace eUseControl.BusinessLogic.DBModel
+{
+    public class ReviewsContext : DbContext
+    {
+        public ReviewsContext() : base("name=eUseControlVillaAgency2")
+        {
+        }
+
+        public virtual DbSet<Review> Reviews { get; set; }
+    }
+}
--- eUseControl.Domain/Entities/User/Review.cs
+++ eUseControl.Domain/Entities/User/Review.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eUseControl.Domain.Entities.User
+{
+    public class Review
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(500, MinimumLength = 5)]
+        public string Content { get; set; }
+
+        public DateTime DatePosted { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual UserProfile User { get; set; }
+    }
+}
